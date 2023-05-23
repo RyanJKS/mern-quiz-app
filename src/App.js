41228422c8bfacadc8d79d20a538b03458bd2abd
RyanJKS <rjhelan@gmail.com>
2023-05-23 01:37:07 +0100

    Layout Complete

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Grid from "@mui/material/Grid";
+import Stack from "@mui/material/Stack";
 import OverviewDialog from "./components/OverviewDialog";
 import LeadershipBoard from "./components/LeadershipBoard";
 import { useContext } from "react";
@@ -32,14 +33,13 @@ function App() {
         <h1 className="title text-primary">Quiz Game</h1>
       </Grid>
       {/*OVERVIEW DIALOG */}
-      <Grid item color="black" style={CenterStyles}>
+      <Grid item color="black" xs={12} lg={12} style={CenterStyles}>
         <OverviewDialog />
       </Grid>
       {/*PARTITION SECTION */}
       <Grid
         container
         item
-        minHeight="78vh"
         direction="row-reverse"
         paddingTop="2rem"
         spacing={2}
@@ -56,17 +56,18 @@ function App() {
           <LeadershipBoard />
         </Grid>
         {/*CHANGING SECTION */}
-        <Grid
-          item
-          xs={12}
-          lg={8}
-          display="flex"
-          flexDirection="column"
-          justifyContent="space-evenly"
-          alignItems="center"
-        >
-          <QuizCard />
-          <RoutingPaths />
+        <Grid item xs={12} lg={8}>
+          <Stack
+            direction="column"
+            justifyContent="center"
+            alignItems="center"
+            spacing={4}
+          >
+            {/*DASHBOARD */}
+            <QuizCard />
+            {/*USER LOCATION */}
+            <RoutingPaths />
+          </Stack>
         </Grid>
       </Grid>
     </Grid>
